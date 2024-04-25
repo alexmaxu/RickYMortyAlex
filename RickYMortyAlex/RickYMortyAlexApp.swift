ba665d9bf31c3f1803aa4447dd37b633e7cf0dca
alexmaxu <12alexmaxu@gmail.com>
2024-04-25 17:29:01 +0200

    Initial Commit

---
+++ RickYMortyAlex/RickYMortyAlexApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickYMortyAlexApp.swift
+//  RickYMortyAlex
+//
+//  Created by Alex  on 25/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RickYMortyAlexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
